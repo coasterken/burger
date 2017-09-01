47a59519f83597860d35a9cee8819591931cefd6
coasterken <ken.r.thompson@icloud.com>
2017-09-01 13:00:53 -0400

    Initial Update

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Insert a set of records.
+
+INSERT INTO burgers (burger_name) VALUES ('Big Sloppy');
+
+INSERT INTO burgers (burger_name) VALUES ('Double Sloppy with Cheese');
+
+INSERT INTO burgers (burger_name) VALUES ('Triple Sloppy with Cheese');
+
+select * from burgers;
